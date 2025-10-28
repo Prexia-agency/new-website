fb397c69520cedd2eb95b0ae7e451fa203dd1f1d
kazvi <avivkazari4@gmail.com>
2025-10-28 14:01:49 +0200

    new online email hover scale 2

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -360,7 +360,7 @@ export default function ContactPage() {
                 <button
                   type="submit"
                   disabled={isSubmitting || !acceptedTerms}
-                  className={`w-full gradient-bg text-white px-4 py-1.5 rounded-[8.5px] font-semibold text-[13px] shadow-lg sm:px-4 sm:py-2.5 sm:text-[16px] relative transition-opacity duration-100 ${
+                  className={`w-full gradient-bg text-white px-4 py-1.5 rounded-[8.5px] font-semibold text-[13px] shadow-lg sm:px-4 sm:py-2.5 sm:text-[16px] relative transition-opacity duration-800 ${
                     isSubmitting || !acceptedTerms ? 'opacity-40 cursor-not-allowed' : 'hover:scale-[1.01] duration-900'
                   }`}
                 >
