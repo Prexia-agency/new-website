c4f1935c100266656f308d1cba6d0702ed88f716
kazvi <avivkazari4@gmail.com>
2025-11-04 09:44:02 +0200

    new GTM tag overall script

--- src/components/GoogleTagManagerClient.tsx
+++ src/components/GoogleTagManagerClient.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { GoogleTagManager } from '@next/third-parties/google';
+import Script from 'next/script';
 import { isAnalyticsEnabled } from '@/utils/cookieConsent';
 
 export default function GoogleTagManagerClient() {
@@ -58,8 +58,24 @@ export default function GoogleTagManagerClient() {
     return null;
   }
 
-  console.log(`[GTM] Rendering GoogleTagManager component with ID: ${gtmId}`);
-  return <GoogleTagManager gtmId={gtmId} />;
+  console.log(`[GTM] Rendering standard GTM snippet with ID: ${gtmId}`);
+  return (
+    <>
+      <Script id="gtm-base" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmId}');`}
+      </Script>
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${gtmId}"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        }}
+      />
+    </>
+  );
 }
 
 
